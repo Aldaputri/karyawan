6f0a56b81d36d6a1b8f098f319308060a96f3189
Aldaputri <aldaputri0032@gmail.com>
2022-05-16 22:46:24 +0800

    Insert data karyawan

--- pages/karyawancreate.php
+++ pages/karyawancreate.php
@@ -3,5 +3,49 @@
         <h1 class="h2">Tambah Karyawan</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
         </div>
+        <?php
+          include_once "../database/database.php";
+
+          if(isset($_POST['button_simpan'])){
+            $nik = $_POST['nik'];
+            $nama_karyawan = $_POST['nama_karyawan'];
+            $jenis_kelamin = $_POST['jenis_kelamin'];
+            $status_menikah = $_POST['status_menikah'];
+  
+            // $insertSQL = "INSERT INTO karyawan VALUES (NULL,'".$nik."','".$nama_karyawan."','".$jenis_kelamin."','".$status_menikah."')";
+            $insertSQL = "INSERT INTO karyawan VALUES (NULL, ?, ?, ?, ?)";
+
+            $database = new Database();
+            $connection = $database->getConnection();
+            $statment = $connection->prepare($insertSQL);
+            $statment->bindParam(1, $nik);
+            $statment->bindParam(2, $nama_karyawan);
+            $statment->bindParam(3, $jenis_kelamin);
+            $statment->bindParam(4, $status_menikah);
+            $statment->execute();
+            
+          }
+        ?>
+      </div>
+      <div>
+        <form action="" method="post">
+          <div class="mb-3">
+            <label for="nik" class="form-label">Nomor Induk Karyawan</label>
+            <input type="text" class="form-control" id="nik" name="nik">
+          </div>
+          <div class="mb-3">
+            <label for="nama_karyawan" class="form-label">Nama karyawan</label>
+            <input type="text" class="form-control" id="nama_karyawan" name="nama_karyawan">
+          </div>
+          <div class="mb-3">
+            <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label>
+            <input type="text" class="form-control" id="jenis_kelamin" name="jenis_kelamin">
+          </div>
+          <div class="mb-3">
+            <label for="status_menikah" class="form-label">Status Menikah</label>
+            <input type="text" class="form-control" id="status_menikah" name="status_menikah">
+          </div>
+          <button class="btn btn-success" type="submit" name="button_simpan"><span data-feather="database"></span> Simpan</button>
+        </form>
       </div>
 </main>
