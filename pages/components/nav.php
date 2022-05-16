b1f6e2d4d2c1952444cf269e53d9bf68b546c3dd
Aldaputri <aldaputri0032@gmail.com>
2022-05-16 22:52:17 +0800

    Menambahkan OR untuk karyawan

--- pages/components/nav.php
+++ pages/components/nav.php
@@ -7,7 +7,7 @@
               Dashboard
             </a>
           <li class="nav-item">
-            <a class="nav-link  <?php echo $_GET['page'] == 'karyawan' ? 'active': '' ?>" href="?page=karyawan">
+            <a class="nav-link  <?php echo $_GET['page'] == 'karyawan' || $_GET['page'] == 'karyawancreate' ? 'active': '' ?>" href="?page=karyawan">
               <span data-feather="users"></span>
               Data Karyawan
             </a>
